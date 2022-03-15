44f2f831ff7c2f39167975c6f211fe49272a7fe6
Hafazfaz <57530041+Hafazfaz@users.noreply.github.com>
2022-03-15 19:50:52 +0100

    commit message on alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,9 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
-* main - prints _putchar
-*
-* Return: 0 (Success)
-*/
-
-int main(void)
-=======
  * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  */
 void print_alphabet(void)
->>>>>>> 94fbb27ed83a4cd1115440a3a27693add321c0d0
 {
 	char ch;
 
